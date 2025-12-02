b2c68be954add686b2c71fc6916e628b619fc917
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2025-12-02 13:49:06 -0800

    Update Day 2/main.py

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,9 +1,11 @@
 def parse_input() -> list:
     with open("Day 2/input.txt") as file:
-        return list of ranges
+        contents = file.read().split(",")
+        for range in contents:
+            range.split("-")
     
 def main():
     ranges = parse_input()
 
-if __name__ == __main__:
+if __name__ == "__main__":
     main()
