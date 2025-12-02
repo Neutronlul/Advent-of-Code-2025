9cc27596489a98153bc990f86e827706049da08f
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2025-12-01 19:14:59 -0800

    Refactor part1 and part2 functions for improved clarity and structure

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,38 +1,54 @@
-dial = 50
-zeros = 0
+def part1(initial_settings):
+    dial = initial_settings["dial"]
+    zeros = initial_settings["zeros"]
+    with open("Day 1/input.txt") as file:
+        for line in file:
+            direction = line[0]
+            clicks = int(line.lstrip("RL"))
+            remainder = clicks % 100
+            if direction == "R":
+                dial += remainder
+            else:
+                dial -= remainder
+            dial = dial % 100
+            if dial == 0:
+                zeros += 1
 
-# with open("Day 1/input.txt") as file:
-#     for line in file:
-#         direction = line[0]
-#         clicks = line.lstrip("RL")
-#         remainder = int(clicks) % 100
-#         if direction == "R":
-#             dial += remainder
-#         else:
-#             dial -= remainder
-#         dial = dial % 100
-#         if dial == 0:
-#             zeros += 1
-#         zeros += int(clicks) / 100
+    print(f"Part 1: {zeros}")
 
 
-with open("Day 1/input.txt") as file:
-    for line in file:
-        direction = line[0]
-        clicks = int(line.lstrip("RL"))
-        if direction == "L":
-            clicks *= -1
+def part2(initial_settings):
+    dial = initial_settings["dial"]
+    zeros = initial_settings["zeros"]
+    with open("Day 1/input.txt") as file:
+        for line in file:
+            direction = line[0]
+            clicks = int(line.lstrip("RL"))
+            if direction == "L":
+                clicks *= -1
 
-        zeros += abs(clicks) // 100
-        clicks = clicks % 100
+            zeros += abs(clicks) // 100
+            clicks = clicks % 100
 
-        final_position = (dial + clicks) % 100
+            final_position = (dial + clicks) % 100
 
-        if direction == "R" and final_position < dial:
-            zeros += 1
-        elif direction == "L" and final_position > dial:
-            zeros += 1
+            if direction == "R" and final_position < dial:
+                zeros += 1
+            elif direction == "L" and final_position > dial:
+                zeros += 1
 
-        dial = final_position
+            dial = final_position
 
-print(zeros)
+    print(f"Part 2: {zeros}")
+
+
+def main():
+    initial_settings = {"dial": 50, "zeros": 0}
+
+    part1(initial_settings)
+
+    part2(initial_settings)
+
+
+if __name__ == "__main__":
+    main()
