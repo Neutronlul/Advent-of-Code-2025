bc021dade75ede223c6553ae67d868444ae4146f
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2025-12-02 13:42:14 -0800

    Update Day 2/main.py

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,2 +1,9 @@
-if __name__ == main:
+def parse_input() -> list:
+    with open("Day 2/input.txt") as file:
+        return list of ranges
+    
+def main():
+    ranges = parse_input()
+
+if __name__ == __main__:
     main()
