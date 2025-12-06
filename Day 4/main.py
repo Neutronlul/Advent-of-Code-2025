d4aed002e527c87d6e5c04a261d99512f3f2aea8
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2025-12-06 08:28:31 -0800

    Day 4 part 2

--- Day 4/main.py
+++ Day 4/main.py
@@ -9,12 +9,10 @@ def parse_input() -> list[list[dict[str, int | bool]]]:
 def increment_neighbors(
     grid: list[list[dict[str, int | bool]]], row: int, col: int
 ) -> None:
-    cells_checked = 0
     for r in range(max(0, row - 1), min(len(grid), row + 2)):
         for c in range(max(0, col - 1), min(len(grid[0]), col + 2)):
-            if r != row or c != col:
+            if grid[r][c]["is_full"] and (r != row or c != col):
                 grid[r][c]["neighbor_count"] += 1
-                cells_checked += 1
 
 
 def build_adjacency_weightings(grid: list[list[dict[str, int | bool]]]) -> None:
@@ -38,12 +36,51 @@ def get_accessible_rolls(grid: list[list[dict[str, int | bool]]]) -> int:
     return accessible_rolls
 
 
+def decrement_neighbors(
+    grid: list[list[dict[str, int | bool]]], row: int, col: int
+) -> None:
+    for r in range(max(0, row - 1), min(len(grid), row + 2)):
+        for c in range(max(0, col - 1), min(len(grid[0]), col + 2)):
+            if grid[r][c]["is_full"] and (r != row or c != col):
+                grid[r][c]["neighbor_count"] -= 1
+
+
+def get_accessible_rolls_recursive(grid: list[list[dict[str, int | bool]]]) -> int:
+    build_adjacency_weightings(grid)
+    total_accessible_rolls = 0
+    pass_num = 0
+
+    while True:
+        accessible_rolls = 0
+
+        for row in enumerate(grid):
+            for cell in enumerate(row[1]):
+                if cell[1]["neighbor_count"] < 4 and cell[1]["is_full"]:
+                    accessible_rolls += 1
+                    cell[1]["is_full"] = False
+                    decrement_neighbors(grid, row[0], cell[0])
+
+        if accessible_rolls == 0:
+            break
+        else:
+            print(f"Pass {pass_num} found {accessible_rolls} accessible rolls.")
+            pass_num += 1
+
+            total_accessible_rolls += accessible_rolls
+
+    return total_accessible_rolls
+
+
 def main():
     data = parse_input()
 
-    number_of_accessible_rolls = get_accessible_rolls(data)
+    print(f"Number of accessible rolls: {get_accessible_rolls(data)}\n")
 
-    print(f"Number of accessible rolls: {number_of_accessible_rolls}")
+    data = parse_input()
+
+    print(
+        f"Total number of accessible rolls (recursive): {get_accessible_rolls_recursive(data)}"
+    )
 
 
 if __name__ == "__main__":
